735262829057a801910872d72c2962418c82043d
Amaia Casas <amaiacss@gmail.com>
2022-11-06 19:58:33 +0100

    Filters

--- backend/app/Config/Filters.php
+++ backend/app/Config/Filters.php
@@ -23,6 +23,7 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'corsFilter' => \App\Filters\CorsFilter::class,
     ];
 
     /**
@@ -33,6 +34,7 @@ class Filters extends BaseConfig
      */
     public $globals = [
         'before' => [
+            'corsFilter'
             // 'honeypot',
             // 'csrf',
             // 'invalidchars',
---
+++ backend/app/Filters/CorsFilter.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class CorsFilter implements FilterInterface {
+
+    public function before(RequestInterface $request, $arguments = null) {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: GET, POST,OPTIONS,PUT, DELETE");
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null) {
+        
+    }
+}
