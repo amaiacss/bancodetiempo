2e0cb56eae88c67542c44e112589a42df13fe895
Amaia Casas <amaiacss@gmail.com>
2022-11-30 18:32:33 +0100

    ajuste

--- backend/app/Config/App.php
+++ backend/app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'https://www.bancodetiempo.es';
 
     /**
      * --------------------------------------------------------------------------
@@ -37,7 +37,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $indexPage = 'index.php';
+    public $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
