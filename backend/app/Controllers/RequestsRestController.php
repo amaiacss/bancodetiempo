db4fea6740c40f355ac088858a7ff533af5712e4
Amaia <61711537+amaiacss@users.noreply.github.com>
2023-01-05 00:44:44 +0100

    Request avances

--- backend/app/Controllers/RequestsRestController.php
+++ backend/app/Controllers/RequestsRestController.php
@@ -38,8 +38,46 @@ class RequestsRestController extends BaseController
         }
         
     }
+    // Se recuperan las solicitudes que se han hecho a otros usuario 
+    // El id que se recibe es el logado
+    public function getRequests($id) {
+        $model = new RequestsModel();
 
-    // cancelar la solicitud
+        $model->select('requests.id, requests.updated_at, requeststates.name_es, requeststates.name_eu, activities.title');
+        $model->where('requests.idUser', $id);
+        $model->join('requeststates', 'requeststates.id = requests.idState');
+        $model->join('activities', 'activities.id = requests.idActivity');
+        $data = $model->findAll();
+        if(count($data) > 0) {
+            $this->SetResult('ok', true);
+        }
+        
+        $this->SetResult('data', $data);
+        return $this->PrintResult();
+    }
 
+    // Se recuperan las solicitudes que han hecho los usuarios a mis actividades
+    // El id que se recibe es el logeado
+    public function getRequestsByactivities($id) {
+        $modelActivity = new \App\Models\ActivityModel();
+        $modelActivity->select('id')->where('idUser', $id)->where('deleted_at', NULL);
+        $activities = $modelActivity->findAll();
+        $data = [];
+        if (count($activities) > 0) {
+            $activitiesid = [];
+            foreach($activities as $key => $value) {
+                array_push($activitiesid, $value->id);
+            }
+            $model = new RequestsModel();
+            $model->select('requests.id, requests.updated_at, requeststates.name_es, requeststates.name_eu, activities.title, users.username');
+            $model->join('requeststates', 'requeststates.id = requests.idState');
+            $model->join('activities', 'activities.id = requests.idActivity');
+            $model->join('users', 'users.id = requests.idUser');
+            $data = $model->whereIn('idActivity', $activitiesid)->find();  
+            $this->SetResult('ok', true);
+        }
 
+        $this->SetResult('data', $data);
+        return $this->PrintResult();
+    } 
 }
